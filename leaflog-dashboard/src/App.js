3ede308f417be453eb2b237802fbec91eddd9096
zaka420 <squidquaplayz@gmail.com>
2026-06-01 13:50:17 +0930

    feat: got water plant functionality working

--- leaflog-dashboard/src/App.js
+++ leaflog-dashboard/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import DashboardCard from './components/DashboardCard';
 import Sidebar from './components/Sidebar';
 
 function App() {
-  const plants = 
+  const [plants, setPlants] = React.useState( 
 [
   {
       "name" : "Rose",
@@ -14,6 +15,7 @@ function App() {
       "commonmistake" : "Overwatering",
       "plantlocation" : "Front Yard",
       "lastwatered" : "2026-05-29",
+      "daystillwater" : 1,
       "timeswatered" : 4,
       "dateadded" : "2026-05-6"
   },
@@ -25,54 +27,85 @@ function App() {
       "commonmistake" : "Too Dry",
       "plantlocation" : "Pond",
       "lastwatered" : "2026-05-29",
+      "daystillwater" : 2,
       "timeswatered" : 4,
       "dateadded" : "2026-05-6"
   },
   {
-      "name" : "Rose",
+      "name" : "Roser",
       "description" : "A rose is either a woody perennial flowering plant of the genus Rosa in the family Rosaceae or the flower it bears.",
       "imagelink" : "https://www.thetutuguru.com.au/wp-content/uploads/2012/08/Pink-Roses.jpg",
       "wateringfrequency" : "Weekly",
       "commonmistake" : "Overwatering",
       "plantlocation" : "Front Yard",
       "lastwatered" : "2026-05-29",
+      "daystillwater" : 3,
       "timeswatered" : 4,
       "dateadded" : "2026-05-6"
   },
   {
-      "name" : "Rose",
+      "name" : "Roses",
       "description" : "A rose is either a woody perennial flowering plant of the genus Rosa in the family Rosaceae or the flower it bears.",
       "imagelink" : "https://www.thetutuguru.com.au/wp-content/uploads/2012/08/Pink-Roses.jpg",
       "wateringfrequency" : "Weekly",
       "commonmistake" : "Overwatering",
       "plantlocation" : "Front Yard",
       "lastwatered" : "2026-05-29",
+      "daystillwater" : 4,
       "timeswatered" : 4,
       "dateadded" : "2026-05-6"
   },
   {
-      "name" : "Rose",
+      "name" : "Rosed",
       "description" : "A rose is either a woody perennial flowering plant of the genus Rosa in the family Rosaceae or the flower it bears.",
       "imagelink" : "https://www.thetutuguru.com.au/wp-content/uploads/2012/08/Pink-Roses.jpg",
       "wateringfrequency" : "Weekly",
       "commonmistake" : "Overwatering",
       "plantlocation" : "Front Yard",
       "lastwatered" : "2026-05-29",
+      "daystillwater" : 5,
       "timeswatered" : 4,
       "dateadded" : "2026-05-6"
   },
   {
-      "name" : "Rose",
+      "name" : "Rosem",
       "description" : "A rose is either a woody perennial flowering plant of the genus Rosa in the family Rosaceae or the flower it bears.",
       "imagelink" : "https://www.thetutuguru.com.au/wp-content/uploads/2012/08/Pink-Roses.jpg",
       "wateringfrequency" : "Weekly",
       "commonmistake" : "Overwatering",
       "plantlocation" : "Front Yard",
       "lastwatered" : "2026-05-29",
+      "daystillwater" : 6,
       "timeswatered" : 4,
       "dateadded" : "2026-05-6"
   }
-]
+]);
+
+const waterPlant = (plantID) =>
+{
+  const newPlants = plants.map((p, i) =>
+  {
+    if(i === plantID)
+    {
+      let newPlant = plants[plantID];
+      newPlant.timeswatered++;
+      let currentDate = new Date(Date.now());
+      newPlant.lastwatered = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate();
+      newPlant.wateringfrequency === "Weekly" ? newPlant.daystillwater = 7 : newPlant.daystillwater = 30;
+      return newPlant;
+    }
+    else
+    {
+      return p;
+    }
+  });
+  newPlants.sort((a, b) => a.daystillwater - b.daystillwater);
+  
+  setPlants(newPlants);
+  // sortPlants();
+
+  console.log(plants[plantID]);
+}
 
   return (
     <div className="App">
@@ -80,19 +113,13 @@ function App() {
       <div>
         <h1>Dashboard</h1>
         <input id="searchBar" placeholder='Search'></input>
-        <div className="DashboardRows">
-        <div className="DashboardColumn">
-          <DashboardCard data={plants[0]} wateringDay={1}/>
-          <DashboardCard data={plants[1]} wateringDay={4}/>
-        </div>
-        <div className="DashboardColumn">
-          <DashboardCard data={plants[0]} wateringDay={2}/>
-          <DashboardCard data={plants[0]} wateringDay={5}/>
-        </div>
-        <div className="DashboardColumn">
-          <DashboardCard data={plants[0]} wateringDay={3}/>
-          <DashboardCard data={plants[0]} wateringDay={6}/>
-        </div>
+        <div className="Dashboard">
+          <DashboardCard data={plants[0]} waterFunction={() => waterPlant(0)}/>
+          <DashboardCard data={plants[1]} waterFunction={() => waterPlant(1)}/>
+          <DashboardCard data={plants[2]} waterFunction={() => waterPlant(2)}/>
+          <DashboardCard data={plants[3]} waterFunction={() => waterPlant(3)}/>
+          <DashboardCard data={plants[4]} waterFunction={() => waterPlant(4)}/>
+          <DashboardCard data={plants[5]} waterFunction={() => waterPlant(5)}/>
       </div>
     </div>
     </div>
--- leaflog-dashboard/src/components/DashboardCard.jsx
+++ leaflog-dashboard/src/components/DashboardCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default function DashboardCard({data, wateringDay})
+export default function DashboardCard({data, waterFunction})
 {
+    
+
     return (
         <div className="dashboard-card">
             <div className="top-half">
@@ -10,14 +12,14 @@ export default function DashboardCard({data, wateringDay})
                 <img src={data.imagelink} id="plantImage"/>
             </div>
             <img src="WaterDrop.png" id="wateringImage"/>
-            <label for="wateringImage" className="editAndWater">Water Plant</label>
+            <label for="wateringImage" className="editAndWater"><a href="#" onClick={waterFunction}>Water Plant</a></label>
             <br/>
             <img src="PlusSign.png" id="editingImage"/>
-            <label for="editingImage" className="editAndWater">Edit Plant</label>
+            <label for="editingImage" className="editAndWater"><a href="#">Edit Plant</a></label>
             <br/>
             <p id="waterProgressText">Water Progress<br/>
             <progress max="100" value="30"/><br/>
-            <span id="waterSoonText">Needs water soon: {wateringDay} {wateringDay == "1" ? "day" : "days"}</span></p>
+            <span id="waterSoonText">Needs water soon: {data.daystillwater} {data.daystillwater == "1" ? "day" : "days"}</span></p>
         </div>
     );
 }
